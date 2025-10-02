358085b4534a33a355becbdb3ef7c7a709a78c13
luwukien <luukien2910@gmail.com>
2025-10-02 23:29:48 +0700

    refactor: refactor code to change format img extension

--- scripts/2_change_format_img.py
+++ scripts/2_change_format_img.py
@@ -1,12 +1,26 @@
 from pathlib import Path
+import os
 
-FOLDER_PATH = Path('data/cleaned_photos') # Sửa lại tên folder của mày ở đây
+FOLDER_PATH = Path('data/raw_photos') 
 
-image_files = list(FOLDER_PATH.glob('*.png')) + list(FOLDER_PATH.glob('*.jpeg'))
+EXTENSIONS_TO_CONVERT = ['.png', '.jpeg', '.webp', '.tiff']
 
-for file_path in image_files:
-    new_file_path = file_path.with_suffix('.jpg')
+print("Starting file change format image----")
+
+image_files = []
+for ext in EXTENSIONS_TO_CONVERT:
+    image_files.extend(list(FOLDER_PATH.rglob(f'*{ext}')))
+
+if not image_files:
+    print("Not found the imgage to need convert")
+else:
     
-    file_path.rename(new_file_path)
+    for file_path in image_files:
+        new_file_path = file_path.with_suffix('.jpg')
+        
+        try:
+            file_path.rename(new_file_path)
+        except Exception as e:
+            print(f"ERROR file {file_path.name}: {e}")
 
-print("\nTo change img from .png and .jpeg to .jpg")
+    print("\nFinished!")
